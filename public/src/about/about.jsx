d9990f93e7a512a06443cb46767b1af47dbce62e
Carlos Wagner <carloswagner@fullgauge.com.br>
2017-11-27 10:09:53 -0200

    teste com git

---
+++ public/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa HIstória</h2>
+        <p>texto de teste.....</p>
+        <h2>Missão e Visão</h2>
+        <p>texto de teste...</p>
+        <h2>Imprensa</h2>
+        <p>texto de teste...</p>
+        </div>
+
+)
