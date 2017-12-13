b72eca1dde5cf3e56cb0877dfbd75a1728ba4e85
wagnersperling <wagnersperling@gmail.com>
2017-12-13 09:30:06 -0200

    - concluido aplicação Todo

--- public/src/todo/todoReducer.js
+++ public/src/todo/todoReducer.js
@@ -8,8 +8,8 @@ const INITIAL_STATE = {
             case 'DESCRIPTION_CHANGED':
             return {...state, description: action.payload}
             case 'TODO_SEARCHED':
-            return { ...state, list: action.payload.data }
-            case 'TODO_ADDED':
+            return { ...state, list: action.payload }
+            case 'TODO_CLEAR':
             return {...state, description: ''}
 
             default:
